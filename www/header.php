a94fe46fa0f1174fceb46039291ba0b14aab3460
Matteo De Lutiis <delutiismatteo@gmail.com>
2021-09-13 19:15:41 -0400

    Ajout du header PHP

---
+++ www/header.php
@@ -0,0 +1,21 @@
+<header>
+		<div class="parallax">
+			<center><b>Pick Your Logo</b></center>
+		</div>
+		
+			
+		<ul id="nav">
+			<li><a class="active" href="index.html">Accueil</a></li>
+			<li><a href="#mission">Notre Mission</a></li>
+			<li><a href="#produits">Nos Produits</a></li>
+			<li><a href="#connexion">Connexion</a></li>
+		</ul>
+
+		<script src="/www/script/menu-sticky.js"></script>
+		  
+        <?php
+            if($_SESSION["authentifie"]){
+                echo $_SESSION["pseudonyme"];
+            }
+        ?>
+</header>
