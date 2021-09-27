4b328a7ebead0420bfbcf9b61067c7eedada42ff
Nicolas-Rimbault <54377194+Nicolas-Rimbault@users.noreply.github.com>
2021-09-27 16:39:44 -0400

    ajout page mission.php avec mission.css

---
+++ www/mission.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PickYourLogo - Notre Mission</title>
+    <link rel="stylesheet" href="/www/style/mission.css">
+</head>
+
+<?php include "header.php"?>
+
+<body>
+    <div class="mission">
+        <div class="information">
+            <div>
+                <img class="image" src="delaware-potatoes-vegetable.jpg" alt="patate">
+            </div>
+            <div>
+                <p class="text">La mission de notre équipe est de proposer des logoset images copyrightés pour faciliter
+                    les jeunes entrepreneursà se lancer dansle monde du travail.<br/><br/>Notre site possède un large choix de
+                    logos customisablesà différents tarifs afin de satisfaire un maximumde clientèle.</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<?php include "footer.php"?>
