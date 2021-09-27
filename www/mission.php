b5c70a917fd3ab23b37c089723acda79cbe3a2c2
Matteo De Lutiis <delutiismatteo@gmail.com>
2021-09-27 19:40:50 -0400

    Modification de la page mission #4

--- www/mission.php
+++ www/mission.php
@@ -6,21 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PickYourLogo - Notre Mission</title>
-    <link rel="stylesheet" href="/www/style/mission.css">
+    <link rel="stylesheet" href="style/mission.css">
 </head>
 
 <?php include "header.php"?>
 
 <body>
     <div class="mission">
+
+        <h1>Notre Mission !</h1>
         <div class="information">
             <div>
-                <img class="image" src="delaware-potatoes-vegetable.jpg" alt="patate">
+                <img class="image" src="/illustration/parallax.jpg" alt="logos">
             </div>
             <div>
-                <p class="text">La mission de notre équipe est de proposer des logoset images copyrightés pour faciliter
-                    les jeunes entrepreneursà se lancer dansle monde du travail.<br/><br/>Notre site possède un large choix de
-                    logos customisablesà différents tarifs afin de satisfaire un maximumde clientèle.</p>
+                <p class="text">La mission de notre équipe est de proposer des logos et des images de droit d'auteur pour faciliter
+                    les jeunes entrepreneurs à se lancer dans le monde du travail.<br/><br/>Notre site possède un large choix de
+                    logos customisables à différents tarifs afin de satisfaire un maximum de clientèle.</p>
             </div>
         </div>
     </div>
