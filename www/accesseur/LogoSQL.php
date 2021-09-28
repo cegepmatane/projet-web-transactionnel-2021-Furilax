cc7fec89bf10198b33086fa8561c4ff534540d96
Brandon Marceau <60437988+MarBzZ@users.noreply.github.com>
2021-09-28 09:26:33 -0400

    Update LogoSQL.php

--- www/accesseur/LogoSQL.php
+++ www/accesseur/LogoSQL.php
@@ -5,9 +5,9 @@ interface LogoSQL
 	public const SQL_LISTE_LOGOS = "SELECT * FROM logo";
 	public const SQL_DETAIL_LOGO = "SELECT * FROM logo WHERE id = :id"; 
 	public const SQL_NOUVEAUTES_LOGOS = "SELECT * FROM logo LIMIT 3";
-	public const SQL_AJOUTER_LOGO = "INSERT INTO logos (nom, auteur, description, prix) VALUE (:nom, :auteur, :description, :prix)";
-	public const SQL_MODIFIER_LOGO = "UPDATE logos SET nom=:nom, auteur=:auteur, description=:description, prix=:prix where id=:id";
-	public const SQL_LIRE_LOGO = "SELECT nom, auteur, description, prix FROM logos WHERE id=:id";
-	public const SQL_SUPPRIMER_LOGO = "DELETE from logos where id=:id";
+	public const SQL_AJOUTER_LOGO = "INSERT INTO logo (nom, auteur, description, prix) VALUE (:nom, :auteur, :description, :prix)";
+	public const SQL_MODIFIER_LOGO = "UPDATE logo SET nom=:nom, auteur=:auteur, description=:description, prix=:prix where id=:id";
+	public const SQL_LIRE_LOGO = "SELECT nom, auteur, description, prix FROM logo WHERE id=:id";
+	public const SQL_SUPPRIMER_LOGO = "DELETE from logo where id=:id";
 }
 ?>
