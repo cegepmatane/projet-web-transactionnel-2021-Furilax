6fa454e2cbc5b4b45cd7de01436537f72ac68f7d
Nicolas-Rimbault <54377194+Nicolas-Rimbault@users.noreply.github.com>
2021-09-28 00:32:06 -0400

    reupload admin

--- www/admin/admin.php
+++ www/admin/admin.php
@@ -1,16 +1,14 @@
 <?php
-include "../accesseur/LogoDAO.php";
+include "accesseur/LogoDAO.php";
 $listeLogos = LogoDAO::listerLogos();
 ?>
 
 <!doctype html>
 <html lang="fr">
-<link rel="stylesheet" href="style/produits.css">
+<link rel="stylesheet" href="produits.css">
 <link rel="stylesheet" href="../footer/footer.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../../../../www/index.css">
-<link rel="stylesheet" href="style/admin.css">
-<link rel="stylesheet" href="style/logos.css">
+<link rel="stylesheet" href="admin.css">
+<link rel="stylesheet" href="logos.css">
 
 <head>
 	<meta charset="utf-8">
@@ -50,51 +48,13 @@ $listeLogos = LogoDAO::listerLogos();
 			</a>
 			<h2><?=formater($logo->nom)?></h2>
 			<p class = "descriptionLogo"><?=formater($logo->description)?></p>
-			<a href="modifier.php?id=<?=$switch['id']?>"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
+			<a href="modifier.php?id=<?=($logo->id)?>"><button type="button" class="btn modifier">Modifier</button></a>
+			<a href="traitement-supprimer.php?id=<?=($logo->id)?>"><button type="button" class="btn supprimer">Supprimer</button></a>
 		</div>
 	</section>
 <?php
     }
 ?>
-
-	<!--<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Synergy STRATA MANAGEMENT">
-			<h2>Logo 1</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>
-		<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Logos Vintage">
-			<h2>Logo 2</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>
-		<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Logos Vintage">
-			<h2>Logo 3</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>
-		<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Logos Vintage">
-			<h2>Logo 4</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>
-		<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Logos Vintage">
-			<h2>Logo 5</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>
-		<div class="produit">
-			<img class="imageProduit" src="pomme_de_terre.jpg" alt="Logos Vintage">
-			<h2>Logo 6</h2>
-			<a href="modifier.php"><button type="button" class="btn modifier">Modifier</button></a>
-			<button type="button" class="btn supprimer">Supprimer</button>
-		</div>-->
-	</section>
 </body>
 <?php include "footer.php"; ?>
 </html>
