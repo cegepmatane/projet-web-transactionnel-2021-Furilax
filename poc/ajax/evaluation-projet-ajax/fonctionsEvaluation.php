6059dd8bb324ba8b62e39332933172b5834ae186
Matteo De Lutiis <delutiismatteo@gmail.com>
2021-11-25 17:16:24 -0500

    Ajout des fonctions d'affichage des infos des logos pour la POC AJAX #36

--- poc/ajax/evaluation-projet-ajax/fonctionsEvaluation.php
+++ poc/ajax/evaluation-projet-ajax/fonctionsEvaluation.php
@@ -1,6 +1,19 @@
 <script type="text/javascript">
 
-    function survolEvaluation(idLogo, evaluation) {
+    function montrerDonneesLogo(url) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                document.getElementById("conteneurListe").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+
+    } 
+
+    function ajouterEvaluation(idLogo, evaluation) {
 
         var xhttp = new XMLHttpRequest();
 
@@ -19,5 +32,36 @@
         var parametres = "index=" + evaluation + "&idLogo=" + idLogo;
         xhttp.send(parametres);
     }
+
+    function evalUtilisateur($idUtilisateur, $idLogo, $conn)
+    {
+        $average = 0;
+        $avgQuery = "SELECT evaluation FROM evaluation WHERE idUtilisateur = '" . $idUtilisateur . "' and idLogo = '" . $idLogo . "'";
+        $total_row = 0;
+        
+        if ($resultat = mysqli_query($conn, $avgQuery)) {
+            $total_row = mysqli_num_rows($resultat);
+        } 
+
+        if ($total_row > 0) {
+            foreach ($resultat as $row) {
+                $average = round($row["evaluation"]);
+            } 
+        } 
+        return $average;
+    }
+    
+    function evalTotal($idLogo, $conn)
+    {
+        $totalVotesQuery = "SELECT * FROM evaluation WHERE idLogo = '" . $idLogo . "'";
+        
+        if ($resultat = mysqli_query($conn, $totalVotesQuery)) {
+            $rowCount = mysqli_num_rows($resultat);
+            mysqli_free_result($resultat);
+        }
+        
+        return $rowCount;
+    }
+
 
 </script>
