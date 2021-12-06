4b18b253ff3c1f7c1824b3a840eb907e09383e51
Matteo De Lutiis <delutiismatteo@gmail.com>
2021-12-05 19:16:26 -0500

    Intégration de l'AJAX #36

--- logos.php
+++ logos.php
@@ -9,28 +9,131 @@ $listeLogos = LogoDAO::listerLogos();
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="style/logos.css?=21">
+
+	<style>
+		ul {
+			margin: 0px;
+			padding: 10px 0px 0px 0px;
+		}
+
+		li.star {
+			list-style: none;
+			display: inline-block;
+			margin-right: 5px;
+			cursor: pointer;
+			color: #9E9E9E;
+		}
+
+		li.star.selected {
+			color: #ff6e00;
+		}
+
+		.row-title {
+			font-size: 20px;
+			color: #00BCD4;
+		}
+
+		.review-note {
+			font-size: 12px;
+			color: #999;
+			font-style: italic;
+		}
+		.row-item {
+			margin-bottom: 20px;
+			border-bottom: #F0F0F0 1px solid;
+		}
+		p.text-address {
+			font-size: 12px;
+		}
+	</style>
 </head>
 
 <?php include "header.php"?>
 
-<section id="conteneurListe">
+<body onload="montrerDonneesLogo('obtenirDonneesLogo.php')">
 
-<h1>Nos Produits</h1>
+	<section id="conteneurListe">
 
-<?php
-        	foreach($listeLogos as $logo){       
-?>
-			<div class="logo">
-				<a href="logo.php?logo=<?=$logo->id?>">
-					<img class="imageLogo" src="illustration/<?=formater($logo->image)?>" alt="">
-				</a>
-				<h2><?=formater($logo->nom)?></h2>
-				<p class = "descriptionLogo"><?=formater($logo->description)?></p>
-			</div>
-	</section>
-<?php
-    }
-?>
+	<h1>Nos Produits</h1>
+
+	<?php
+				foreach($listeLogos as $logo){       
+	?>
+				<div class="logo">
+					<a href="logo.php?logo=<?=$logo->id?>">
+						<img class="imageLogo" src="illustration/<?=formater($logo->image)?>" alt="">
+					</a>
+					<h2><?=formater($logo->nom)?></h2>
+					<p class = "descriptionLogo"><?=formater($logo->description)?></p>
+					<span id="listeLogo"></span>
+				</div>
+		</section>
+	<?php
+		}
+	?>
+
+	<script type="text/javascript">
+		function montrerDonneesLogo(url) {
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function () {
+				if (this.readyState == 4 && this.status == 200)
+				{
+					document.getElementById("listeLogo").innerHTML = this.responseText;
+				}
+			};
+			xhttp.open("GET", url, true);
+			xhttp.send();
+    	} 
+
+		function ajouterEvaluation(idLogo, evaluation) {
+			var xhttp = new XMLHttpRequest();
+
+			xhttp.onreadystatechange = function() {
+				if(this.readyState == 4 && this.status == 200) {
+					montrerDonneesLogo('obtenirDonneesLogo.php');
+
+					if(this.responseText != "success") {
+						alert(this.responseText);
+					}
+				}
+			};
+
+			xhttp.open("POST", "ajoutEvaluation.php", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			var parametres = "index=" + evaluation + "&idLogo=" + idLogo;
+			xhttp.send(parametres);
+		}
+
+		function evalUtilisateur($idUtilisateur, $idLogo, $conn)
+		{
+			$average = 0;
+			$avgQuery = "SELECT evaluation FROM evaluation WHERE idUtilisateur = '" . $idUtilisateur . "' and idLogo = '" . $idLogo . "'";
+			$total_row = 0;
+			
+			if ($resultat = mysqli_query($conn, $avgQuery)) {
+				$total_row = mysqli_num_rows($resultat);
+			} 
+
+			if ($total_row > 0) {
+				foreach ($resultat as $row) {
+					$average = round($row["evaluation"]);
+				} 
+			} 
+			return $average;
+		}
+    
+		function evalTotal($idLogo, $conn)
+		{
+			$totalVotesQuery = "SELECT * FROM evaluation WHERE idLogo = '" . $idLogo . "'";
+			
+			if ($resultat = mysqli_query($conn, $totalVotesQuery)) {
+				$rowCount = mysqli_num_rows($resultat);
+				mysqli_free_result($resultat);
+			}
+			
+			return $rowCount;
+		}
+	</script>
 
 </body>
 
